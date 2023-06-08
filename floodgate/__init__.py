017b76906a88954fc5ff1c9d8da0fc1c48927cd5
bswck <bswck.dev@gmail.com>
2023-06-08 02:07:00 +0200

    Add documentation (#5)

--- floodgate/__init__.py
+++ floodgate/__init__.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-
-from floodgate.floodgate import FixedMapping, DynamicMapping, JumpingWindow
+from floodgate.floodgate import DynamicMapping, FixedMapping, JumpingWindow
 
 __all__: list[str] = ["FixedMapping", "DynamicMapping", "JumpingWindow"]
